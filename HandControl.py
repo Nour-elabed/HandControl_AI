95b9ffb9fc11c436ce771c2b7c72398944454316
Nour1717 <elabed.nour@gmail.com>
2026-04-25 18:28:18 +0100

    final updates

--- HandControl.py
+++ HandControl.py
@@ -13,6 +13,7 @@ absl.logging.set_verbosity(absl.logging.ERROR)
 sys.stdout.reconfigure(encoding="utf-8", errors="replace") 
 
 
+
 pyautogui.FAILSAFE = False # désactive la sécurité de déplacement de la souris dans un coin
 pyautogui.PAUSE = 0.05  # stabilité des actions avec delai
 
@@ -20,7 +21,7 @@ pyautogui.PAUSE = 0.05  # stabilité des actions avec delai
 # ── Camera ───────────────────────────────
 cap = cv2.VideoCapture(1, cv2.CAP_MSMF) # Essaie d'abord de se connecter à la caméra secondaire (index 1) pour éviter les conflits avec d'autres applications qui pourraient utiliser la caméra principale (index 0). cv2.CAP_MSMF est un backend de capture vidéo pour Windows qui peut offrir de meilleures performances et une meilleure compatibilité avec certaines caméras.
 if not cap.isOpened():
-    
+
 
     cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
 if not cap.isOpened():
